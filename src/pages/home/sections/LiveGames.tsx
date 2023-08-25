8c1c5d24963a32be31509272df8bc0ba8fdbe52f
suprabathk <suprabathkondapally@gmail.com>
2023-08-25 14:50:49 +0530

    Changed Live match indicator

--- src/pages/home/sections/LiveGames.tsx
+++ src/pages/home/sections/LiveGames.tsx
@@ -12,32 +12,34 @@ const LiveGames = () => {
     <div>
       <p className="font-bold text-xl mb-3">Live Games</p>
       {isError && <p className="text-red-500">{errorMessage}</p>}
-      {isLoading && (
-        <div className="flex overflow-x-auto gap-2 p-1">
-          {[...Array(10).keys()].map((id) => (
+      <div className="flex overflow-x-auto gap-2 p-3 rounded-md bg-slate-200 dark:bg-gray-800">
+        {isLoading &&
+          [...Array(10).keys()].map((id) => (
             <div
               key={id}
-              className={`flex-shrink-0 bg-slate-200 dark:bg-gray-800 p-3 rounded-md outline-blue-300 h-28 w-60 animate-pulse`}
+              className={`flex-shrink-0 bg-white p-3 rounded-md outline-blue-300 h-24 w-60 animate-pulse`}
             />
           ))}
-        </div>
-      )}
-      <div className="flex overflow-x-auto gap-2 p-1 -mr-8">
         {matches &&
           matches.reverse().map((match: MatchPreview) => (
             <Link
               to={`/match/${match.id}`}
               key={match.id}
-              className={`flex-shrink-0 bg-gray-200 dark:bg-gray-800 p-3 rounded-md outline-blue-300 dark:outline-white ${
-                match.isRunning && "outline"
-              }`}
+              className="flex-shrink-0 bg-white p-3 rounded-md outline-blue-300 dark:outline-white"
             >
               <div className="flex justify-between items-center mb-3 gap-6">
                 <p className="text-sm">{match.sportName}</p>
-                <div className="flex items-center text-sm text-gray-500 gap-1 dark:text-gray-300">
-                  <CalendarDaysIcon className="w-4 h-4" />
-                  <p>{new Date(match.endsAt).toDateString()}</p>
-                </div>
+                {match.isRunning ? (
+                  <div className="flex items-center gap-1">
+                    <span className="p-1 rounded-full bg-blue-400 animate-pulse" />
+                    <p className="text-blue-400 text-sm">Live now</p>
+                  </div>
+                ) : (
+                  <div className="flex items-center text-sm text-gray-500 gap-1 dark:text-gray-300">
+                    <CalendarDaysIcon className="w-4 h-4" />
+                    <p>{new Date(match.endsAt).toDateString()}</p>
+                  </div>
+                )}
               </div>
               <div className="flex mt-2 items-center font-semibold gap-2">
                 <span>{match.teams[0].name}</span>
